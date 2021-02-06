4068cb8f9d6cdf4ce9a4b2a6533fde848c91bf6f
SergGrey <sergdiag19@gmail.com>
2021-02-06 18:03:47 +0300

    footer fix

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import inst from "../../assets/IconsFooter/instagram.svg"
 import linked from "../../assets/IconsFooter/linkedin.svg"
 import telegram from "../../assets/IconsFooter/telegram.svg"
 import phone from "../../assets/IconsFooter/whatsapp.svg"
+import gmail from "../../assets/IconsFooter/gmail.svg"
 
 export const Footer = () => {
 	const item = [
@@ -11,6 +12,7 @@ export const Footer = () => {
 		{id:2, link: "https://www.linkedin.com/in/sergey-dziatkovich-6006651b8/", icon: linked},
 		{id:3, link: "tg://resolve?domain=@SergDia", icon: telegram},
 		{id:4, link: "tel:+375291577837", icon: phone},
+		{id:5, link: "mailto:sergdiag19@gmail.com", icon: gmail},
 	]
 	const itemsMap = item.map( i => <div key={i.id}><a key={i.id} href={i.link}><img key={i.id} src={i.icon} alt="#"/></a></div> )
 	return (
